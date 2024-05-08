3791faa0d8eaf9ba10bd97618a3b61e8ac6aa0e4
sam <sfriedman6711@gmail.com>
2024-05-08 01:37:33 -0600

    group tiles

--- lib/widgets/group_tile.dart
+++ lib/widgets/group_tile.dart
@@ -18,9 +18,25 @@ class GroupTile extends StatefulWidget {
 class _GroupTileState extends State<GroupTile> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(widget.groupId),
-      subtitle: Text(widget.groupName),
-    );
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundColor: const Color.fromARGB(255, 101, 88, 221),
+            child: Text(
+              widget.groupName.substring(0, 1).toUpperCase(),
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w500),
+            ),
+          ),
+          title: Text(
+            widget.groupName,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text("Join the conversation as ${widget.userName}",
+              style: const TextStyle(fontSize: 13)),
+        ));
   }
 }
